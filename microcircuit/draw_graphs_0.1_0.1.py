cbc9a88f4d4dd4ad96ebc0393defe51a5a0c120c
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2018-06-18 10:54:01 +0100

    Tidy up

--- microcircuit/draw_graphs_0.1_0.1.py
+++
@@ -1,7 +0,0 @@
-from ana_vistools import data
-from matplotlib import pyplot
-
-vis = data(simtime=1000., n_scaling=0.1)
-vis.load_spikes(rec_type="frac", rec=1.0, dir="results_0.1_0.1")
-vis.dot_display()
-pyplot.show()
--- microcircuit/results/sim_script.sh
+++
@@ -1,8 +0,0 @@
-
-#PBS -o results/output.txt
-#PBS -e results/errors.txt 
-#PBS -l walltime=8:0:0
-#PBS -l nodes=1:ppn=24
-#PBS -l mem=4gb
-. /usr/local/mpi/openmpi/1.4.3/gcc64/bin/mpivars_openmpi-1.4.3_gcc64.sh
-mpirun -np 24 python results/microcircuit.py
