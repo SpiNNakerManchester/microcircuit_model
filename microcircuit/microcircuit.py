0a1de07d550c4548e07df598993907ddf112ed05
oliverrhodes <oliver.rhodes@manchester.ac.uk>
2019-01-08 11:46:03 +0000

    Revert "update data write to fix use in plotting.py"
    This reverts commit a2e175c8f954122d19f6d7c854d57c351e66d77b.

--- microcircuit/microcircuit.py
+++ microcircuit/microcircuit.py
@@ -71,7 +71,7 @@ if record_v:
     for layer in layers:
         for pop in pops:
             filename = system_params['output_path'] + '/voltages_' + layer + pop + '.dat'
-	    n.pops[layer][pop].write_data(io=filename, variables='spikes')
+            n.pops[layer][pop].print_v(filename, gather=True)
 
 if simulator == 'nest':
     if record_corr:
