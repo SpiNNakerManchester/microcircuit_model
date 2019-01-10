57a2088fa16cf6adcd083a79fdaa47bf583bf9a8
Andrew Gait <andrew.gait@manchester.ac.uk>
2019-01-10 14:57:39 +0000

    Minor edits to make it work using python3 (and still work in python2)

--- microcircuit/microcircuit.py
+++ microcircuit/microcircuit.py
@@ -21,8 +21,8 @@ sim.setup(**simulator_params[simulator])
 if simulator == 'nest':
     n_vp = sim.nest.GetKernelStatus('total_num_virtual_procs')
     if sim.rank() == 0:
-        print 'n_vp: ', n_vp
-        print 'master_seed: ', master_seed
+        print('n_vp: ', n_vp)
+        print('master_seed: ', master_seed)
     sim.nest.SetKernelStatus({'print_time' : False,
                               'dict_miss_is_error': False,
                               'grng_seed': master_seed,
@@ -40,26 +40,26 @@ n = network.Network(sim)
 n.setup(sim)
 end_netw = time.time()
 if sim.rank() == 0:
-    print 'Creating the network took ', end_netw - start_netw, ' s'
+    print( 'Creating the network took ', end_netw - start_netw, ' s')
 
 if simulator == 'nest':
     # determine memory consumption
     sim.nest.sli_run("memory_thisjob")
-    print 'memory usage after network creation:', sim.nest.sli_pop(), 'kB'
+    print('memory usage after network creation:', sim.nest.sli_pop(), 'kB')
 
 # simulate
 if sim.rank() == 0:
-    print "Simulating..."
+    print("Simulating...")
 start_sim = time.time()
 t = sim.run(simulator_params[simulator]['sim_duration'])
 end_sim = time.time()
 if sim.rank() == 0:
-    print 'Simulation took ', end_sim - start_sim, ' s'
+    print('Simulation took ', end_sim - start_sim, ' s')
 
 if simulator == 'nest':
     # determine memory consumption
     sim.nest.sli_run("memory_thisjob")
-    print 'memory usage after simulation:', sim.nest.sli_pop(), 'kB'
+    print('memory usage after simulation:', sim.nest.sli_pop(), 'kB')
 
 start_writing = time.time()
 for layer in layers:
@@ -76,7 +76,7 @@ if record_v:
 if simulator == 'nest':
     if record_corr:
         if sim.nest.GetStatus(n.corr_detector, 'local')[0]:
-            print 'getting count_covariance on rank ', sim.rank()
+            print('getting count_covariance on rank ', sim.rank())
             cov_all = sim.nest.GetStatus(n.corr_detector, 'count_covariance')[0]
             delta_tau = sim.nest.GetStatus(n.corr_detector, 'delta_tau')[0]
 
@@ -92,9 +92,9 @@ if simulator == 'nest':
                             + list(cov_all[source_index][target_index][1:]))
 
             f = open(system_params['output_path'] + '/covariances.dat', 'w')
-            print >>f, 'tau_max: ', tau_max
-            print >>f, 'delta_tau: ', delta_tau
-            print >>f, 'simtime: ', simulator_params[simulator]['sim_duration'], '\n'
+            f.write('tau_max: ', tau_max)
+            f.write('delta_tau: ', delta_tau)
+            f.write('simtime: ', simulator_params[simulator]['sim_duration'], '\n')
 
             for target_layer in np.sort(layers.keys()):
                 for target_pop in pops:
@@ -102,17 +102,17 @@ if simulator == 'nest':
                     for source_layer in np.sort(layers.keys()):
                         for source_pop in pops:
                             source_index = structure[source_layer][source_pop]
-                            print >>f, target_layer, target_pop, '-', source_layer, source_pop
-                            print >>f, 'n_events_target: ', sim.nest.GetStatus(n.corr_detector, 'n_events')[0][target_index]
-                            print >>f, 'n_events_source: ', sim.nest.GetStatus(n.corr_detector, 'n_events')[0][source_index]
+                            f.write(target_layer, target_pop, '-', source_layer, source_pop)
+                            f.write('n_events_target: ', sim.nest.GetStatus(n.corr_detector, 'n_events')[0][target_index])
+                            f.write('n_events_source: ', sim.nest.GetStatus(n.corr_detector, 'n_events')[0][source_index])
                             for i in xrange(len(cov[target_index][source_index])):
-                                print >>f, cov[target_index][source_index][i]
-                            print >>f, ''
+                                f.write(cov[target_index][source_index][i])
+                            f.write('')
             f.close()
 
 
 end_writing = time.time()
-print "Writing data took ", end_writing - start_writing, " s"
+print("Writing data took ", end_writing - start_writing, " s")
 
 if plot_spiking_activity and sim.rank()==0:
     plotting.plot_raster_bars( \
